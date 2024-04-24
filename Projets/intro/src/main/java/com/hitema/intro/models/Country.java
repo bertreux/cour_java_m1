faa4d294b7799e2af57a0148c31a33aa2e4165ac
bertreux <115898406+bertreux@users.noreply.github.com>
2024-04-24 10:12:38 +0200

    change capital to string

--- Projets/intro/src/main/java/com/hitema/intro/models/Country.java
+++ Projets/intro/src/main/java/com/hitema/intro/models/Country.java
@@ -31,7 +31,6 @@ public class Country {
                 "id=" + id +
                 ", name='" + name + '\'' +
                 ", lastUpdate=" + lastUpdate +
-                ", cities=" + cities.toString() +
                 '}';
     }
 
