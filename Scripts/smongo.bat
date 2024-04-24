b7a547f6aee621a64ee5d81b66b16e90d8a224fb
bertreux <115898406+bertreux@users.noreply.github.com>
2024-04-24 11:39:02 +0200

    Correction script port +100

--- Scripts/smongo.bat
+++ Scripts/smongo.bat
@@ -4,7 +4,7 @@
 @echo OFF
 set OPTION=%1
 if %OPTION%. == start. (
-	docker run -p27018:27017 --rm --name mongodb -d ^
+	docker run -p27117:27017 --rm --name mongodb -d ^
     			-e MONGO_INITDB_ROOT_USERNAME=java1 ^
     			-e MONGO_INITDB_ROOT_PASSWORD=java1 ^
     			-v C:\Users\bertr\Documents\h3hitema\M1\JEE\Datas\MongoDB:/data/db mongo
