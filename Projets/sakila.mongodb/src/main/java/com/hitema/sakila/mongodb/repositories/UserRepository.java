e8166480f5422ade2c22b04fef33fbe709ee3fce
bertreux <115898406+bertreux@users.noreply.github.com>
2024-04-24 15:35:32 +0200

    Enlever import inutile

--- Projets/sakila.mongodb/src/main/java/com/hitema/sakila/mongodb/repositories/UserRepository.java
+++ Projets/sakila.mongodb/src/main/java/com/hitema/sakila/mongodb/repositories/UserRepository.java
@@ -2,7 +2,6 @@ package com.hitema.sakila.mongodb.repositories;
 
 import com.hitema.sakila.mongodb.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
